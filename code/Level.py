2931e6be46f4ab96d800f05413ad2d042c2908bd
Vitor Pereira <vitor.pereira8@hotmail.com>
2026-02-14 13:25:34 -0300

    Creating players and enemies Creating players and enemies

--- code/Level.py
+++ code/Level.py
@@ -1,3 +1,4 @@
+import random
 import sys
 
 import pygame.display
@@ -5,7 +6,7 @@ from pygame.font import Font
 from pygame.rect import Rect
 from pygame.surface import Surface
 
-from code.Const import COLOR_WHITE, WIN_WIDTH, WIN_HEIGHT
+from code.Const import COLOR_WHITE, WIN_WIDTH, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, SPAWN_TIME
 from code.Entity import Entity
 from code.EntityFactory import EntityFactory
 
@@ -17,6 +18,10 @@ class Level:
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity("Level1Bg"))
+        self.entity_list.append(EntityFactory.get_entity("Player1"))
+        if game_mode in [MENU_OPTION[1], MENU_OPTION[2]]:
+            self.entity_list.append(EntityFactory.get_entity("Player2"))
+        pygame.time.set_timer(EVENT_ENEMY, SPAWN_TIME)
         self.timeout = 20000
 
     def run(self):
@@ -31,6 +36,9 @@ class Level:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                if event.type == EVENT_ENEMY:
+                    choice = random.choice(("Enemy1", "Enemy2"))
+                    self.entity_list.append(EntityFactory.get_entity(choice))
 
             self.level_text(14, f"{self.name} - Timeout: {self.timeout / 1000 : .1f}s", COLOR_WHITE, (10, 5))
             self.level_text(14, f"fps: {clock.get_fps() :.0f}", COLOR_WHITE, (10, WIN_HEIGHT - 35))
